5d7780678993fe21ae2633324c012a1236c1f781
solieth <soliethv@uamv.edu.ni>
2024-09-27 08:32:12 -0600

    "clase":

---
+++ Sesion12/models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sesion12.models
+{
+     public struct Product
+    {
+        public int Id;
+        public string Name;
+    }
+}
